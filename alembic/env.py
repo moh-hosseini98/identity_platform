9d12cb43919932fd53a04925c988e79bdb16946b
فناوری اطلاعات 25 <anf_ope25@Notary.org>
2026-06-30 10:58:37 +0330

    add todo model and migrate

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,9 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+
+
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
