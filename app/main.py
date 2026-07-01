22b92aefe7a259c084e4983bacd7b9848333386d
فناوری اطلاعات 25 <anf_ope25@Notary.org>
2026-07-01 13:33:11 +0330

    chnage main.py for new test in prod

--- app/main.py
+++ app/main.py
@@ -28,4 +28,4 @@ app.include_router(v1_router)
 
 @app.get("/health", tags=["Health"])
 async def health():
-    return {"status": "ok (test for ci cd in production)"}
+    return {"status": "ok (test for ci cd in production v2)"}
